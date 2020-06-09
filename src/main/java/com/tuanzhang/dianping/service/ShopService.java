efeea52fda05c247290eaef0ec6a0cde25078748
330059579 <wyunlong@yeah.net>
2020-06-09 21:16:30 +0800

    commit

--- src/main/java/com/tuanzhang/dianping/service/ShopService.java
+++ src/main/java/com/tuanzhang/dianping/service/ShopService.java
@@ -3,6 +3,7 @@ package com.tuanzhang.dianping.service;
 import com.tuanzhang.dianping.common.BusinessException;
 import com.tuanzhang.dianping.model.Shop;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
@@ -24,4 +25,6 @@ public interface ShopService {
     List<Shop> search(BigDecimal longitude, BigDecimal latitude, String keyword, Integer orderBy, Integer categoryId, String tags);
 
     List<Map<String, Object>> searchGroupByTags(String keyword, Integer categoryId, String tags);
+
+    Map<String, Object> searchES(BigDecimal longitude, BigDecimal latitude, String keyword, Integer orderBy, Integer categoryId, String tags) throws IOException;
 }
